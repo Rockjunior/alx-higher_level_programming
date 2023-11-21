47d5c13e76034f8fbcb2eb3a0a561d3434a639b6
Roman <108798583+Rockjunior@users.noreply.github.com>
2023-11-21 11:59:42 +0300

    feelin and f*cking out like moses

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py
+# festus sulumeti
+"""Define a new class: """
+
+
+class Square(object):
+    """A simple example class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+# festus sulumeti
+"""Private instance attribute: """
+
+class Square(object):
+    """class variable size"""
+    def __init__(self, size):
+        """initialize size"""
+        self.__size = size
